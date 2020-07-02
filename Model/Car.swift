8e40d5d7f60362eca95a84097e0dafad58f94964
rui-watanabe <wataru610806@gmail.com>
2020-07-02 22:30:36 +0900

    first commit

---
+++ Model/Car.swift
@@ -0,0 +1,10 @@
+//
+//  Car.swift
+//  Swift5CalcApp
+//
+//  Created by watar on 2020/07/02.
+//  Copyright © 2020 rui watanabe. All rights reserved.
+//
+
+import Foundation
+
---
+++ ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Swift5CalcApp
+//
+//  Created by watar on 2020/07/02.
+//  Copyright © 2020 rui watanabe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+}
+
